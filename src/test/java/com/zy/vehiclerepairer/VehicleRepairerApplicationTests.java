f409a30859b7a878380a81fe1c9baf3fcbed400c
543400086 <543400086@qq.com>
2018-08-24 00:19:16 +0800

    添加mybatis自动生成工具

--- src/test/java/com/zy/vehiclerepairer/VehicleRepairerApplicationTests.java
+++ src/test/java/com/zy/vehiclerepairer/VehicleRepairerApplicationTests.java
@@ -1,15 +1,10 @@
 package com.zy.vehiclerepairer;
 
-import com.zy.vehiclerepairer.controller.PersonController;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
